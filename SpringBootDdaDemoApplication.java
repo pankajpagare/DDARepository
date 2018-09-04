a0e439f20a219de28f3e8ec65ebc923455db693f
pankajpagare <42965614+pankajpagare@users.noreply.github.com>
2018-09-04 16:46:29 +0530

    Add files via upload

---
+++ SpringBootDdaDemoApplication.java
@@ -0,0 +1,12 @@
+package com.SpringBootDemo.SpringBootDDADemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDdaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDdaDemoApplication.class, args);
+	}
+}
